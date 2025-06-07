fd666762a4f01f80b97ffffc3153e798ada280d7
Marcucci-Francesco <fmarcucci47@gmail.com>
2025-06-07 12:32:44 +0200

    [feat] add milestone 2

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,5 +1,9 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { useState } from 'react'
+
+const letters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()-_=+[]{}|;:'\",.<>?/`~";
 
 const Main = () => {
 
@@ -12,7 +16,7 @@ const Main = () => {
 
   const handlerSubmit = (e) => {
     e.preventDefault()
-    if (!fullName || !userName || !password || !specialization || !experience || !description) {
+    if (!fullName.trim() || !userName.trim() || !password.trim() || !specialization.trim() || !experience.trim() || !description.trim() || !isUsernameValid || !isPasswordValid || !isDescriptionValid) {
       alert('Tutti i campi sono obbligatori!')
       return
     }
@@ -25,6 +29,26 @@ const Main = () => {
       description
     });
   }
+
+  const isUsernameValid = useMemo(() => {
+    const charsValid = userName.split('').every(char => {
+      letters.includes(char.toLowerCase()) || numbers.includes(char);
+    })
+    return charsValid && userName.trim().length >= 6;
+  }, [userName]);
+
+  const isPasswordValid = useMemo(() => {
+    return (
+      password.trim().length >= 8 &&
+      password.split('').some(char => letters.includes(char.toLowerCase())) &&
+      password.split('').some(char => numbers.includes(char)) &&
+      password.split('').some(char => symbols.includes(char))
+    )
+  }, [password]);
+
+  const isDescriptionValid = useMemo(() => {
+    return description.trim().length >= 100 && description.trim().length < 1000;
+  }, [description]);
 
   return (
     <div>
@@ -46,6 +70,11 @@ const Main = () => {
               value={userName}
               onChange={(e) => { setUserName(e.target.value) }}
               className="form-control" />
+            {userName.trim() && (
+              <p style={{ color: isUsernameValid ? 'green' : 'red' }}>
+                {isUsernameValid ? 'Username valido' : 'Username non valido, deve contenere almeno 6 caratteri alfanumerici'}
+              </p>
+            )}
           </label>
           <label className="form-label mx-4 col-3 mb-4">
             <p>Password</p>
@@ -54,6 +83,11 @@ const Main = () => {
               value={password}
               onChange={(e) => { setPassword(e.target.value) }}
               className="form-control" />
+            {password.trim() && (
+              <p style={{ color: isPasswordValid ? 'green' : 'red' }}>
+                {isPasswordValid ? 'Password valida' : 'Password non valida, deve contenere almeno 8 caratteri, con almeno 1 numero ed 1 simbolo'}
+              </p>
+            )}
           </label>
           <label className="form-label col-3 mb-4">
             <p>Specializzazione</p>
@@ -84,6 +118,11 @@ const Main = () => {
               className="form-control"
               rows={4}
             />
+            {description.trim() && (
+              <p style={{ color: isDescriptionValid ? 'green' : 'red' }}>
+                {isDescriptionValid ? 'Descrizione valida' : `Descrizione non valida, deve contenere tra 100 e 1000 caratteri (${description.trim().length} caratteri attuali)`}
+              </p>
+            )}
           </label>
           <button type='submit' className='btn btn-primary col-3 mx-2'>Registrati</button>
         </form>
